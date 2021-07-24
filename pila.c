e6e266c1dea47bff9cbe6e4f05365287966a8fda
DarkStar <david.mattonelli@gmail.com>
2021-07-24 12:44:01 +0200

    Add function prototpe

--- pila.c
+++ pila.c
@@ -1,1 +1,18 @@
-/* GESTIONE DI UNA pila */
+/* GESTIONE DI UNA pila 
+ * 
+ * Operazioni di inserimento, eliminazione e visualizzazione.
+ * Utilizza un array di strutture per implementare la pila
+ * */
+
+#define LEN_STACK 10
+
+void handleStack(void);
+int * insert ( int *, int *, int);
+int * delete (int *, int *, int *);
+int isEmpty(int);
+void showStack(int *, int);
+
+int main(){
+	
+	return 0;
+}
